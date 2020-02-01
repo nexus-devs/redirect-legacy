bce596f063b5b6c41a85e5522a5232eeb291f85a
Kaptard <kilian@nexus-stats.com>
2020-02-01 12:28:20 +0100

    fix: Use 301 redirect status code

--- index.js
+++ index.js
@@ -7,39 +7,39 @@ const suggestions = 'https://api.nexushub.co/warframe/v1/suggestions?query='
 const format = str => str.split(' ').join('-').toLowerCase()
 const missing = {}
 
-app.get('/', (req, res) => res.status(301).redirect(`${url}/`))
+app.get('/', (req, res) => res.redirect(301, `${url}/`))
 
-app.get('/NexusBot', (req, res) => res.status(301).redirect(`${url}/`))
+app.get('/NexusBot', (req, res) => res.redirect(301, `${url}/`))
 
-app.get('/feedback', (req, res) => res.status(301).redirect(`https://nexushub.co/contact`))
+app.get('/feedback', (req, res) => res.redirect(301, `https://nexushub.co/contact`))
 
-app.get('/how_we_get_our_stats', (req, res) => res.status(301).redirect(`https://nexushub.co/opensource`))
+app.get('/how_we_get_our_stats', (req, res) => res.redirect(301, `https://nexushub.co/opensource`))
 
-app.get('/privacy_policy', (req, res) => res.status(301).redirect(`https://nexushub.co/privacy`))
+app.get('/privacy_policy', (req, res) => res.redirect(301, `https://nexushub.co/privacy`))
 
-app.get('/categories', (req, res) => res.status(301).redirect('https://nexushub.co/warframe/search?input=nova%20prime'))
-app.get('/prime', (req, res) => res.status(301).redirect('https://nexushub.co/warframe/search?input= prime'))
-app.get('/syndicate', (req, res) => res.status(301).redirect('https://nexushub.co/warframe/search?input=vaykor'))
-app.get('/special', (req, res) => res.status(301).redirect('https://nexushub.co/warframe/search?input=nezha'))
-app.get('/arcane', (req, res) => res.status(301).redirect('https://nexushub.co/warframe/search?input=arcane'))
-app.get('/prisma', (req, res) => res.status(301).redirect('https://nexushub.co/warframe/search?input=prisma'))
-app.get('/mods', (req, res) => res.status(301).redirect('https://nexushub.co/warframe/search?input=maim'))
+app.get('/categories', (req, res) => res.redirect(301, 'https://nexushub.co/warframe/search?input=nova%20prime'))
+app.get('/prime', (req, res) => res.redirect(301, 'https://nexushub.co/warframe/search?input= prime'))
+app.get('/syndicate', (req, res) => res.redirect(301, 'https://nexushub.co/warframe/search?input=vaykor'))
+app.get('/special', (req, res) => res.redirect(301, 'https://nexushub.co/warframe/search?input=nezha'))
+app.get('/arcane', (req, res) => res.redirect(301, 'https://nexushub.co/warframe/search?input=arcane'))
+app.get('/prisma', (req, res) => res.redirect(301, 'https://nexushub.co/warframe/search?input=prisma'))
+app.get('/mods', (req, res) => res.redirect(301, 'https://nexushub.co/warframe/search?input=maim'))
 
 app.get('/:category/:item', async (req, res) => {
   const category = req.params.category.toLowerCase()
   const item = req.params.item
 
   if (category === 'prime') {
-    res.status(301).redirect(`${url}/items/${format(item + ' ' + category)}/prices`)
+    res.redirect(301, `${url}/items/${format(item + ' ' + category)}/prices`)
   }
   else if (category === 'kavat') {
-    res.status(301).redirect(`${url}/items/${format(item + ' ' + category)}`)
+    res.redirect(301, `${url}/items/${format(item + ' ' + category)}`)
   }
   else if (category === 'special') {
-    res.status(301).redirect(`${url}/items/${format(item)}`)
+    res.redirect(301, `${url}/items/${format(item)}`)
   }
   else if (category === 'arcane') {
-    res.status(301).redirect(`${url}/`)
+    res.redirect(301, `${url}/`)
   }
   else if (item.includes('primed')) {
     if (!missing[item]) {
@@ -47,16 +47,16 @@ app.get('/:category/:item', async (req, res) => {
 
       if (full[0]) {
         missing[item] = format(full[0].name)
-        res.status(301).redirect(`${url}/items/${missing[item]}/prices`)
+        res.redirect(301, `${url}/items/${missing[item]}/prices`)
       } else {
-        res.status(301).redirect(`${url}/`)
+        res.redirect(301, `${url}/`)
       }
     } else {
-      res.status(301).redirect(`${url}/items/${format(missing[item])}/prices`)
+      res.redirect(301, `${url}/items/${format(missing[item])}/prices`)
     }
   }
   else {
-    res.status(301).redirect(`${url}/items/${format(item)}/prices`)
+    res.redirect(301, `${url}/items/${format(item)}/prices`)
   }
 })
 
